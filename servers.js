5af390d4448728b6a2468bbf943a136603988c5f
Aungthu772 <aungthuarun@gmail.com>
2018-01-18 23:40:25 +0630

    initial commit

---
+++ servers.js
@@ -0,0 +1,52 @@
+const express = require('express');
+
+const hbs =require('hbs');
+
+const app = express();
+
+hbs.registerPartials(__dirname+'/views/partials');
+
+hbs.registerHelper('date',()=>{
+  return new Date().getFullYear();
+})
+
+hbs.registerHelper('big',(text)=>{
+  return text.toUpperCase();
+})
+
+
+app.set('view engine','hbs');
+
+app.use((req,res,next)=>{
+
+  var now = new Date().toString();
+
+  var log=`${req.method} ${req.url} ${now} `
+  console.log(log);
+  next();
+})
+
+app.use((req,res,next)=>{
+  res.render('maintence.hbs');
+})
+
+app.use(express.static(__dirname+'/Playground'));
+
+
+app.get('/',(req,res)=>{
+  res.render('about.hbs',{
+    welcomemessage:'This is welcome meeage bro'
+  })
+})
+
+app.get('/help',(req,res)=>{
+  res.render('help.hbs');
+})
+
+app.get('/helper',(req,res)=>{
+  res.send('helper.html');
+})
+
+app.listen(3000,()=>{
+  console.log('port 3000');
+})
